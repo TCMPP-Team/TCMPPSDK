6556eaec2271ee5a5f02153ac3dc42fae8fc7f66
stonelshi <stonelshi@tencent.com>
2025-07-03 21:01:20 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
         .package(url: "https://github.com/CoderMJLee/MJRefresh.git", from: "3.7.9"),
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", .upToNextMinor(from: "2.4.3")),
         .package(url: "https://github.com/TCMPP-Team/Brotli.git", from: "1.1.0"),
-        .package(url: "https://github.com/TCMPP-Team/MQQComponents.git", from: "2.0.0"),
         .package(url: "https://github.com/TCMPP-Team/PromiseObjC.git", from: "0.7.1"),
         .package(url: "https://github.com/TCMPP-Team/SocketRocket.git", from: "0.5.1"),
         .package(url: "https://github.com/TCMPP-Team/Tars.git", from: "1.6.0"),
@@ -24,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TCMPPSDK",
-            url: "https://tmf-warehouse-1314481471.cos.ap-beijing.myqcloud.com/tcmpp/ios-sdk/noshark/TCMPPSDK/2.2.10/4/TCMPPSDK_2.2.10.xcframework.zip",
-                        checksum: "b455f04d58bf4637e7ec18a77e8e9bc9ab3f1da758ff8beee77a51f3195d55d3"
+            url: "https://tmf-warehouse-1314481471.cos.ap-beijing.myqcloud.com/tcmpp/ios-sdk/noshark/TCMPPSDK/2.2.10/14/TCMPPSDK_2.2.10.xcframework.zip",
+                        checksum: "c965a286a9cc9349c6da0f939166aeebe0c26860c12a9b180d5e719af98a4b8f"
         ),
         .target(
             name: "TCMPPSDKWrapper",
@@ -34,7 +33,6 @@ let package = Package(
                 .product(name: "MJRefresh", package: "MJRefresh"),
                 .product(name: "ZipArchive", package: "ZipArchive"),
                 .product(name: "Brotli", package: "Brotli"),
-                .product(name: "MQQComponents", package: "MQQComponents"),
                 .product(name: "PromiseObjC", package: "PromiseObjC"),
                 .product(name: "SocketRocket", package: "SocketRocket"),
                 .product(name: "Tars", package: "Tars"),
