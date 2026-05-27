9aceffa61c39399d09a38b4bbfb14eabc51c3ec2
TCMPP-Team <tcmpp_superapp@tencent.com>
2026-05-27 12:16:07 +0800

    2.3.6

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TCMPPSDK",
-            url: "https://tmf-warehouse-1314481471.cos.ap-beijing.myqcloud.com/tcmpp/ios-sdk/noshark/TCMPPSDK/2.3.8/34/TCMPPSDK_2.3.8.xcframework.zip",
-                        checksum: "72572be15a47ed7d9a6022e8d370509fdac8db9f969fbd8a1cb40af3692480ff"
+            url: "https://tmf-warehouse-1314481471.cos.ap-beijing.myqcloud.com/tcmpp/ios-sdk/noshark/TCMPPSDK/2.3.6/31/TCMPPSDK_2.3.6.xcframework.zip",
+                        checksum: "fa97a68520a151c6821926857540203ff62d7c00d16cff7bf6adc070062efe13"
         ),
         .target(
             name: "TCMPPSDKWrapper",
